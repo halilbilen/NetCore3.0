36bf26252d26dc98e3e2f1f7cf1aff3f8836fd1b
halil bilen <halil.bilen.1@hotmail.com>
2020-01-15 18:31:52 +0300

    Business

---
+++ NorthWind/Business/Abstract/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        Product GetById(Product ProductId);
+        List<Product> GetList();
+        List<Product> GetListByCategory(int CategoryId);
+
+        void Add(Product Product);
+        void Delete(Product Product);
+        void Update(Product Product);
+    }
+}
---
+++ NorthWind/Business/Concrete/ProductManager.cs
@@ -0,0 +1,41 @@
+﻿using Business.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        public void Add(Product Product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product Product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetById(Product ProductId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetListByCategory(int CategoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product Product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
