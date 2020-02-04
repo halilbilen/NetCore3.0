79b41a03e268b5e2b7a8d843a723c928a63cdb45
Halil Bilen <38647437+halilbilen@users.noreply.github.com>
2020-02-04 11:26:59 +0300

    OperationClaims

---
+++ NorthWind/Entity/Concrete/UserOperationClaims.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entity.Concrete
+{
+    public class UserOperationClaims : IEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int OperationClaimId { get; set; }
+    }
+}
