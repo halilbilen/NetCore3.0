715b666fc289bffedc5002549bfaaf8f3213eb59
Halil Bilen <38647437+halilbilen@users.noreply.github.com>
2020-02-10 11:59:04 +0300

    AuthManager

---
+++ NorthWind/Business/Concrete/AuthManager.cs
@@ -0,0 +1,49 @@
+﻿using Business.Abstract;
+using Business.Contants;
+using Core.Entity.Concrete;
+using Core.Utilities.Results;
+using Core.Utilities.Security.JsonWebToken;
+using Entity.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class AuthManager : IAuthService
+    {
+        private IUserService userService;
+        private ITokenHelper tokenHelper;
+
+        public AuthManager(IUserService _userService, ITokenHelper _tokenHelper)
+        {
+            userService = _userService;
+            tokenHelper = _tokenHelper;
+        }
+
+        public IDataResult<AccessToken> CreateAccessToken(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<User> Login(UserForLoginDto userForLoginDto)
+        {
+            var userCheck = userService.GetByMail(userForLoginDto.Email);
+            if (userCheck == null)
+            {
+                return new ErrorDataResult<User>(Messages.UserNotFound);
+            }
+
+        }
+
+        public IDataResult<User> Register(UserForRegisterDto userForRegisterDto, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult UserExists(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- NorthWind/Business/Contants/Messages.cs
+++ NorthWind/Business/Contants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entity.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -13,5 +14,7 @@ namespace Business.Contants
         public static string CategoryAdded = "Basarili Ekleme";
         public static string CategoryDeleted = "Basarili Silindi";
         public static string CategoryUpdated = "Basarili Guncellendi";
+
+        public static string UserNotFound = "Kullanıcı Bulunamadi";
     }
 }
